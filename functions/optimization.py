7f1bc20eae82241db087ca6efad221c16fb50bed
Roman Gorbunov <romangorbunov91@yandex.ru>
2025-09-23 09:15:11 +0700

    v0.3.3
    dfp works well

--- functions/optimization.py
+++ functions/optimization.py
@@ -1,5 +1,5 @@
-# version 0.3.2 by romangorbunov91
-# 22-Sep-2025
+# version 0.3.3 by romangorbunov91
+# 23-Sep-2025
 import numpy as np
 from scipy.linalg import cho_factor, cho_solve
 from functions.functions_under_study import predict, loss_func, grad_func, hess_func, jacob_func
@@ -75,7 +75,6 @@ def gauss_newton(X, y, w_init, tolerance):
     hess_counter = 0    
     return w, losses, i+1, func_counter, grad_counter, hess_counter, jacob_counter
 
-
 # Davidon–Fletcher–Powell.
 def dfp(X, y, w_init, tolerance):
     iteration_max = int(1e4)
@@ -101,16 +100,16 @@ def dfp(X, y, w_init, tolerance):
         learning_rate = 1.0
         p = H_inv @ grad
         
-        dw = learning_rate * p
-        w_test = w - dw
+        dw = -learning_rate * p
+        w_test = w + dw
         new_loss = loss_func(X, y, w_test)
         func_counter += 1
         
         grad_dot_dw = np.dot(grad, dw)
         while ((loss - new_loss) < lr_coeff * learning_rate * grad_dot_dw) and (learning_rate > eps_zero):
             learning_rate *= lr_multiplier
-            dw = learning_rate * p
-            w_test = w - dw
+            dw = -learning_rate * p
+            w_test = w + dw
             new_loss = loss_func(X, y, w_test)
             func_counter += 1
         
@@ -134,9 +133,9 @@ def dfp(X, y, w_init, tolerance):
             if abs(denom) > eps_zero:
                 H_inv += (np.outer(dw, dw) / dwdgrad - np.outer(Hdgrad, Hdgrad) / denom)
             else:
-                print(f"Iter {i+1}: denominator near zero, skipping H_inv update.")
+                print('Iteration', i+1, ': denominator near zero, skipping H_inv update.')
         else:
-            print(f"Iter {i+1}: Curvature condition violated (sᵀy = {dwdgrad:.2e}), resetting H_inv.")
+            print(f'Iteration {i+1}: Curvature condition violated (dw dgrad = {dwdgrad:.2e}) resetting H_inv to identity.')
             # Reset to identity.
             H_inv = np.eye(len(w))
 
@@ -146,55 +145,3 @@ def dfp(X, y, w_init, tolerance):
     hess_counter = 0
     jacob_counter = 0  
     return w, losses, i+1, func_counter, grad_counter, hess_counter, jacob_counter
-
-'''
-Hy = H_inv @ dgrad
-#H_inv -= (np.outer(Hy, Hy) / np.dot(dgrad, Hy) - np.outer(dw, dw) / np.dot(dw, dgrad))
-H_inv = H_inv - np.outer(Hy, Hy) / np.dot(dgrad, Hy) + np.outer(dw, dw) / np.dot(dw, dgrad)
-'''
-
-
-'''
-prod = np.dot(dw, dgrad)
-if prod <= eps_zero:
-    while prod <= eps_zero:
-        print('condition', dw, dgrad, prod)
-        
-        learning_rate *= lr_multiplier
-        dw = -learning_rate * p
-        
-        grad_prev = grad.copy()
-        grad = grad_func(X, y, w+dw)
-        grad_counter += 1
-
-        dgrad = grad - grad_prev
-        
-        p = H_inv @ grad
-        
-        prod = np.dot(dw, dgrad)
-        #loss = loss_func(X, y, w)
-        #losses.append(loss)
-        #func_counter += 1
-        #continue
-'''
-
-'''
-grad_norm = np.linalg.norm(grad, ord=None, axis=None)
-while (loss - loss_func(X, y, w - learning_rate * grad)) < lr_coeff * learning_rate * grad_norm**2:
-    func_counter += 1
-    learning_rate *= lr_multiplier
-# One more increment because of 'while'.
-func_counter += 1
-'''
-
-'''
-# BFGS (Broyden–Fletcher–Goldfarb–Shanno
-def bfgs(X, y, w_init, tolerance):
-
-    # BFGS update:
-    # H_{k+1} = (I - ρ s y^T) H_k (I - ρ y s^T) + ρ s s^T, где ρ = 1/(y^T s)
-    rho = 1.0 / np.dot(y_vec, s)
-    I = np.eye(n_params)
-    V = I - rho * np.outer(s, y_vec)
-    H_inv = V.T @ H_inv @ V + rho * np.outer(s, s)
-'''
